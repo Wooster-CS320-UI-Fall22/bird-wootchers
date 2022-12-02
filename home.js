4f4408116395a55cacc72996f36645d7aa81a486
Taemour Zaidi <tzaidi24@wooster.edu>
2022-12-02 16:47:40 -0500

    reogranized files

---
+++ home.js
@@ -0,0 +1,22 @@
+function toDexNumber(str) {
+    while(str.length < 3) {
+        str = '0' + str;
+    }
+    return str;
+}
+function randomNumber() {
+    var num = Math.floor(Math.random() * (151 - 1) + 1);
+    console.log(num.toString());
+    dexNum = toDexNumber(num.toString());
+
+    var missingno = Math.floor(Math.random() * (1000 - 1) + 1);
+    if (missingno == 1) {
+        return '000';
+    }
+
+    return dexNum;
+}
+var numDebug = randomNumber()
+console.log(numDebug)
+
+document.getElementById("randpoke").src = "data/sprites/" + numDebug + ".png";
